fda5abf5e86e58a218bf2344ca395460b0b2f7a5
MaksimSav <savenkovmax2002@yandex.ru>
2021-10-22 13:29:17 +0600

    Init project

---
+++ SimpleEngineCore/includes/SimpleEngineCore/Utils/test.hpp
@@ -0,0 +1,4 @@
+namespace SimpleEngine {
+
+	void sayHello();
+}
---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <SimpleEngineCore/Utils/test.hpp>
+
+int main()
+{
+	std::cout << "Hello from SimpleEngineEditor" << std::endl;
+	SimpleEngine::sayHello();
+	std::cin.get();
+}
